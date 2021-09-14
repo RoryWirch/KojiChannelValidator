d9f54e308e58b129fa6b5ad80c77c3a21baa09bd
Rory Wirch <rwirch@redhat.com>
2021-09-14 12:20:55 -0400

    collect task IDs and check for scratch builds

--- enum_channels.py
+++ enum_channels.py
@@ -1,9 +1,5 @@
-import os
-import requests
 import koji
-
-
-
+from pprint import pprint
 
 mykoji = koji.get_profile_module('brew')
 
@@ -14,11 +10,36 @@ channels = session.listChannels()
 
 for channel in channels:
     channel_name = channel['name']
-    
     hosts = session.listHosts(channelID=channel_name)
 
     print(f"\nchannel name: {channel_name}")
 
     for host in hosts:
-        host_name = host['name']
-        print(f"\t{host_name}")
+        #check if host is enabled
+        if host['enabled']:
+            host_name = host['name']
+            host_id = host['id']
+            print(f"\t{host_name}")
+            print(f"\thostID: {host_id}")
+
+            opts = {
+                'host_id': host_id,
+                'method': 'buildArch',
+                'state': [koji.TASK_STATES['CLOSED']],
+                'decode': 'True'
+            }
+            queryOpts={'limit':1, 'order':'-completion_time'}
+
+            tasks = session.listTasks(opts, queryOpts)
+
+            # check if there are tasks returned for the host
+            if len(tasks) > 0:
+                parent_id = tasks[0]['parent']
+                build_info = session.listBuilds(taskID=parent_id)
+
+                # check for scratch build
+                if len(build_info) == 0: 
+                    print(f"\t\tscratch build ID: {parent_id}")
+                
+            else:
+                print(f"\tNo tasks found on {host_name}:{host_id}")
